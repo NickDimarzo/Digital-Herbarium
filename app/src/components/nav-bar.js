c88a724b1775e63344c8dc29516b6033a8307e97
NickDimarzo <124927183+NickDimarzo@users.noreply.github.com>
2024-05-20 19:07:10 -0600

    started changing to new color theme

--- app/src/components/nav-bar.js
+++ app/src/components/nav-bar.js
@@ -7,20 +7,20 @@ export default function NavBar() {
   const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-moss border-4 border-dark xl:text-2xl shadow-dark">
+    <nav className="navbar navbar-expand-lg navbar-light bg-dark-blue border-4 border-dark-blue xl:text-2xl shadow-dark-blue">
       <div className="flex justify-between">
         <div className="flex justify-center">
-        <div className="bg-dark text-brick font-mono m-2 px-4 rounded-lg shadow-dark">
+        <div className="bg-dark-green text-gray-50 font-mono m-2 px-4 rounded-lg shadow-dark-blue">
           <h1 className="m-2">Digital Herbarium</h1>
         </div>
-        <div className="bg-dark text-brick font-mono w-max m-2 px-4 rounded-lg hover:bg-velvet transition duration-500 hover:scale-110">
+        <div className="bg-dark-green text-gray-50 font-mono w-max m-2 px-4 rounded-lg hover:bg-light-green transition duration-500 hover:scale-110">
           <button className="flex justify-center p-2">
             <Link href="collection">My Collection</Link>
           </button>
         </div>
         </div>
         <div className="flex justify-center">
-          <div className="bg-dark text-brick font-mono w-max m-2 px-4 rounded-lg hover:bg-velvet transition duration-500 hover:scale-110">
+          <div className="bg-dark-green text-gray-50 font-mono w-max m-2 px-4 rounded-lg hover:bg-light-green transition duration-500 hover:scale-110">
             <button onClick={firebaseSignOut} className="flex items-center h-full">
               Sign Out
             </button>
