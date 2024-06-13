df82114e7ac13945bc0275af88f0239dae0bdee7
NickDimarzo <124927183+NickDimarzo@users.noreply.github.com>
2024-06-13 14:21:16 -0600

    reworked landing page

--- app/src/components/feature-card.jsx
+++ app/src/components/feature-card.jsx
@@ -1,17 +1,17 @@
 export default function FeatureCard( {header, img, text}) {
   return (
-    <div className="flex-col w-1/3 flex items-center bg-darker-blue m-5 text-white rounded-2xl shadow-2xl shadow-black">
+    <div className="flex-col w-1/2 xl:w-1/3 flex items-center bg-darker-blue m-2 text-white rounded-2xl shadow-2xl shadow-black">
       <div className="flex items-center">
-        <h1 className="text-5xl mt-4 ">{header}</h1>
+        <h1 className=" text-2xl xl:text-4xl mt-4 ">{header}</h1>
       </div>
-      <div className=" w-full h-full flex justify-center">
+      <div className=" w-full flex justify-center p-2">
         <img
-          className="object-cover rounded-2xl my-2 shadow-2xl shadow-black "
+          className="object-cover rounded-2xl my-2 w-48 h-48"
           src={img}
         />
       </div>
       <div className="flex items-center justify-center p-4 m-4 bg-light-green rounded-2xl ">
-        <p className="text-3xl">
+        <p className=" text-xl xl:text-2xl">
             {text}
         </p>
       </div>
