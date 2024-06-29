7ad3cce7e8e9bd3953ec10397f9c1119d04e1350
NickDimarzo <124927183+NickDimarzo@users.noreply.github.com>
2024-06-28 20:33:11 -0600

    nav bar formatting

--- app/src/components/nav-bar.js
+++ app/src/components/nav-bar.js
@@ -7,15 +7,15 @@ export default function NavBar() {
   const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-dark-blue border-4 border-dark-blue xl:text-2xl shadow-dark-blue">
+    <nav className="navbar navbar-expand-lg navbar-light bg-dark-blue border-4 border-dark-blue shadow-dark-blue xl:text-2xl text-sm">
       <div className="flex justify-between">
         <div className="flex justify-center">
-        <div className="bg-dark-green text-gray-50 font-mono m-2 px-4 rounded-lg hover:bg-light-green transition duration-500 hover:scale-110 ">
-          <button className="flex justify-center p-2">
+        <div className="bg-dark-green text-gray-50 font-mono m-2 sm:px-4 rounded-lg hover:bg-light-green transition duration-500 hover:scale-110 ">
+          <button className="flex justify-center p-2 items-center">
             <Link href="/src">Digital Herbarium</Link>
           </button>
         </div>
-        <div className="bg-dark-green text-gray-50 font-mono w-max m-2 px-4 rounded-lg hover:bg-light-green transition duration-500 hover:scale-110">
+        <div className="bg-dark-green text-gray-50 font-mono w-max m-2 sm:px-4 rounded-lg hover:bg-light-green transition duration-500 hover:scale-110">
           <button className="flex justify-center p-2">
             <Link href="/src/collection">My Collection</Link>
           </button>
