6c1697de2d2cefc4619799242d598596bc6bafc5
NickDimarzo <124927183+NickDimarzo@users.noreply.github.com>
2024-11-11 16:33:35 -0700

    resources

--- app/src/pages/resources/page.js
+++ app/src/pages/resources/page.js
@@ -1,8 +1,21 @@
+"use client";
+
+import { useState } from "react";
+import { useUserAuth } from "../../_utils/auth-context";
+import NavBar from "../../components/nav-bar";
 
 export default function Page() {
   return (
-    <div>
-      <h1>This is the resources page</h1>
-    </div>
+    <>
+      <header>
+        <nav>
+          <NavBar />
+        </nav>
+      </header>
+      <main>
+        <h1>Resources</h1>
+        <p>Here are some resources to help you get started with your plant collection.</p>
+      </main>
+    </>
   );
 }
