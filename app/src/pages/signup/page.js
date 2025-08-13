61ddf9dbd656ed3e7428dc371ee0f2fd96d300f1
NickDimarzo <124927183+NickDimarzo@users.noreply.github.com>
2025-08-13 09:17:41 -0600

    small ui changes

--- app/src/pages/signup/page.js
+++ app/src/pages/signup/page.js
@@ -5,8 +5,7 @@ import LoadingBlock from "./components/loading-block";
 import NewUserBlock from "./components/newUser-block";
 
 export default function Page() {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
+  const [fullName, setFullName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
@@ -49,7 +48,7 @@ export default function Page() {
     <main className="w-screen h-full flex-col justify-center">
       <div className="flex justify-center">
         <div className="custom-card xl:w-1/3">
-          <div className="flex-col justify-center m-8 font-mono bg-white p-5 rounded-3xl text-sm  m:text-lg lg:text-xl xl:text-2xl">
+          <div className="flex-col justify-center m-8 font-mono bg-white p-5 rounded-3xl">
             {loading ? (
               <LoadingBlock />
             ) : user ? (
@@ -57,55 +56,38 @@ export default function Page() {
             ) : (
               <div className="flex flex-col items-center">
                 <div className="flex justify-center items-center">
-                  <h1>Welcome to account</h1>
+                  <h1>Create Account</h1>
                 </div>
-                <div className="flex justify-center items-center">
-                  <h1>Sign up</h1>
-                </div>
-                {/* <div className="flex justify-center w-2/3">
-                  <p className="py-4">
-                    Please fill out the form below to create an account
-                  </p>
-                </div> */}
                 <div className="flex-col justify-center m-4 w-full">
                   <form onSubmit={handleSubmit}>
-                    <div className="flex flex-col justify-end my-4 w-full p-4">
-                      <label className="w-full">First Name:</label>
+                    <div className="flex flex-col justify-end w-full mt-4">
+                      <label className="w-full">Full name:</label>
                       <input
                         type="email"
-                        value={firstName}
-                        onChange={(e) => setFirstName(e.target.value)}
-                        className=" text-black m-2 w-full border-b-2 border-dark-blue outline-none"
+                        value={fullName}
+                        onChange={(e) => setFullName(e.target.value)}
+                        className=" text-black w-full border-b-2 border-dark-blue outline-none"
                       />
                     </div>
-                    <div className="flex flex-col justify-end my-4 w-full p-4">
-                      <label className="w-full">Last Name:</label>
-                      <input
-                        type="email"
-                        value={lastName}
-                        onChange={(e) => setLastName(e.target.value)}
-                        className=" text-black m-2 w-full border-b-2 border-dark-blue outline-none"
-                      />
-                    </div>
-                    <div className="flex flex-col justify-end my-4 w-full p-4">
+                    <div className="flex flex-col justify-end w-full mt-4">
                       <label className="w-full">Email:</label>
                       <input
                         type="email"
                         value={email}
                         onChange={(e) => setEmail(e.target.value)}
-                        className=" text-black m-2 w-full border-b-2 border-dark-blue outline-none"
+                        className=" text-black w-full border-b-2 border-dark-blue outline-none"
                       />
                     </div>
-                    <div className="flex flex-col justify-end my-4 w-full p-4">
+                    <div className="flex flex-col justify-end w-full mt-4">
                       <label className="w-full">Password:</label>
                       <input
                         type="password"
                         value={password}
                         onChange={handlePasswordChange}
-                        className=" text-black m-2 w-full border-b-2 border-dark-blue outline-none"
+                        className=" text-black w-full border-b-2 border-dark-blue outline-none"
                       />
                     </div>
-                    <div className="flex justify-end text-sm lg:text-lg">
+                    <div className="flex text-sm lg:text-lg">
                       <small
                         style={{
                           color:
@@ -113,21 +95,21 @@ export default function Page() {
                               ? "green"
                               : "red",
                         }}
-                        className="w-1/2 flex justify-end"
+                        className="flex"
                       >
                         {passwordMessage}
                       </small>
                     </div>
-                    <div className="flex flex-col justify-end my-4 w-full p-4">
+                    <div className="flex flex-col justify-end w-full mt-4">
                       <label className="w-full">Confirm Password:</label>
                       <input
                         type="password"
                         value={confirmPassword}
                         onChange={handleConfirmPasswordChange}
-                        className=" text-black m-2 w-full border-b-2 border-dark-blue outline-none"
+                        className=" text-black w-full border-b-2 border-dark-blue outline-none"
                       />
                     </div>
-                    <div className="flex justify-end text-sm lg:text-lg ">
+                    <div className="flex text-sm lg:text-lg ">
                       <small
                         style={{
                           color:
